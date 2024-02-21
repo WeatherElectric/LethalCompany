7520a90c757a56f2cdd815116cc51d4ad54de1e6
Mae <mae@weatherelectric.xyz>
2024-02-21 10:14:13 -0800

    Did thing

--- MeetAndHuh/Monobehaviours/ShutUpMae.cs
+++ MeetAndHuh/Monobehaviours/ShutUpMae.cs
@@ -1,17 +1,20 @@
-﻿using UnityEngine;
+﻿using Unity.Netcode;
+using UnityEngine;
 
 namespace MeetAndHuh.Monobehaviours
 {
     public class ShutUpMae : GrabbableObject
     {
         public AudioSource audioSource;
+        private readonly NetworkVariable<bool> _isPlaying = new(true, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
         
         public override void ItemActivate(bool used, bool buttonDown = true)
         {
             base.ItemActivate(used, buttonDown);
             if (!(GameNetworkManager.Instance.localPlayerController == null))
             {
-                audioSource.mute = !audioSource.mute;
+                _isPlaying.Value = !_isPlaying.Value;
+                audioSource.mute = !_isPlaying.Value;
             }
         }
     }
